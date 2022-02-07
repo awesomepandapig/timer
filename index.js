45943c3f1595b28e8e215334cfca79b4fa7121d6
Lior Subotnick <theawesomepandapig@gmail.com>
2022-02-06 17:10:16 -0800

    update

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
+var noSleep = new NoSleep();
+
 var seconds = 0;
 var minutes = 0;
 var hours = 0;
 
 function run() {
-    async function getScreenLock() {
-        if(isScreenLockSupported()){
-          let screenLock;
-          try {
-             screenLock = await navigator.wakeLock.request('screen');
-          } catch(err) {
-             console.log(err.name, err.message);
-          }
-          return screenLock;
-        }
-    }
     document.body.style.cursor = "none";
     setInterval(function () {
-        if (seconds == 0) {
-            seconds = 59;
-            if (minutes > 0) {minutes--};
-        }
-        if (minutes == 0) {
-            minutes = 59;
-            if (hours > 0) {hours--};
-        }
         let tempSeconds = seconds;
         let tempMinutes = minutes;
         let tempHours = hours;
@@ -35,10 +18,21 @@ function run() {
 }
 
 window.onload = function() {
-    setInterval(function () {
+    enableNoSleep();
+    getTime = setInterval(function () {
         const d = new Date();
         seconds = 60 - d.getSeconds();
         minutes = 60 - d.getMinutes() - 1;
+        if (seconds == 60) {
+            seconds = 0;
+        }
+        if (minutes == 60) {
+            minutes = 0;
+        }
         run();
     }, 1);
 };
+
+function enableNoSleep() {
+  noSleep.enable();
+}
